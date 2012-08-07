9df357813b9bf06a5e29b3c051e7a13b5624e0fd
Andrew Yates <andrewyates.name@gmail.com>
2012-08-07 18:01:03 -0400

    working boxplotters

--- normdata_workflow.m
+++ normdata_workflow.m
@@ -1,14 +1,14 @@
 function normdata_workflow(tabname)
-display sprintf('Loading %s', tabname)
+disp(sprintf('Loading %s', tabname))
 M = importfile(tabname);
 q = maboxplot(M);
-print('-depsc','-tiff','-r300',tabname);
-print('-dpng','-r300',tabname);
-// display sprintf('Quantile Norming %s', tabname)
-// M = quantilenorm(M);
-// q = maboxplot(M);
-// print('-depsc','-tiff','-r300', strcat(tabname,'.normed'));
-// print('-dpng','-r300', strcat(tabname,'.normed'));
-// outname = strcat(tabname, '.normed');
-// display sprintf('Saving quantile-normed to %s', outname)
-// save outname M
+print('-depsc','-tiff','-r300', sprintf('%s.eps',tabname));
+print('-dpng','-r300', sprintf('%s.png',tabname));
+% display sprintf('Quantile Norming %s', tabname)
+% M = quantilenorm(M);
+% q = maboxplot(M);
+% print('-depsc','-tiff','-r300', strcat(tabname,'.normed'));
+% print('-dpng','-r300', strcat(tabname,'.normed'));
+% outname = strcat(tabname, '.normed');
+% display sprintf('Saving quantile-normed to %s', outname)
+% save outname M
