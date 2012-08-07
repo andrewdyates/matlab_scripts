2d17f2934317464ed189d6177d673728ed566718
Andrew Yates <andrewyates.name@gmail.com>
2012-08-07 18:53:59 -0400

    working workflow

---
+++ README.md
@@ -0,0 +1,5 @@
+Quantile normalize a .tab text array in MATLAB.
+
+EXAMPLE USE:
+  cd /path/to/matlab_scripts
+  matlab -nodisplay -nosplash -nodesktop -logfile "~/mylog.txt" -r "normdata_workflow('~/data/GSE7307.raw.tab'); exit"
--- normdata_workflow.m
+++ normdata_workflow.m
@@ -1,14 +1,21 @@
 function normdata_workflow(tabname)
-disp(sprintf('Loading %s', tabname))
+% Quantile normalize a tab-delimited matrix; save boxplots.
+% Files saved to same path as `tabname`
+
+disp(sprintf('Loading %s...', tabname))
 M = importfile(tabname);
 q = maboxplot(M);
+disp(sprintf('Plotting %s.eps and %s.png...', tabname, tabname))
 print('-depsc','-tiff','-r300', sprintf('%s.eps',tabname));
-print('-dpng','-r300', sprintf('%s.png',tabname));
-% display sprintf('Quantile Norming %s', tabname)
-% M = quantilenorm(M);
-% q = maboxplot(M);
-% print('-depsc','-tiff','-r300', strcat(tabname,'.normed'));
-% print('-dpng','-r300', strcat(tabname,'.normed'));
-% outname = strcat(tabname, '.normed');
-% display sprintf('Saving quantile-normed to %s', outname)
-% save outname M
+print('-dpng', sprintf('%s.png',tabname));
+
+disp(sprintf('Quantile Norming %s...', tabname))
+M = quantilenorm(M);
+q = maboxplot(M);
+disp(sprintf('Plotting %s.normed.eps and %s.normed.png...', tabname, tabname))
+print('-depsc','-tiff','-r300', sprintf('%s.normed.eps',tabname));
+print('-dpng', sprintf('%s.normed.png',tabname));
+
+outname = sprintf('%s.normed.mat',tabname);
+disp(sprintf('Saving quantile-normed to %s', outname))
+save(outname, 'M')
